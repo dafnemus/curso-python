a144723777784b325216839863f8d2ed3641bd3e
dafnemus <dafnemus@gmail.com>
2020-09-22 16:58:30 -0300

    emprolije el ejercicio integrador

--- Python/clase8/integrador.py
+++ Python/clase8/integrador.py
@@ -8,37 +8,37 @@ class Punto:
     def __str__(self):
         puntos = '(' + str(self.x) + ',' + str(self.y) + ')'
         print(puntos)
-    
+    print()
     def cuadrante(self):
         x = self.x
         y = self.y
 
         if x == 0 and y == 0:
-            return 'Origen'
+            print('Origen')
         elif x != 0 and y == 0:
-            return 'Eje X'
+            print('Eje X')
         elif x == 0 and y != 0:
-            return 'Eje Y'
+            print('Eje Y')
         elif x > 0 and y > 0:
-            return 'Cuadrante 1'
+            print('Cuadrante 1')
         elif x < 0 and y > 0:
-            return 'Cuadrante 2'
+            print('Cuadrante 2')
         elif x < 0 and y < 0:
-            return 'Cuadrante 3'
+            print('Cuadrante 3')
         elif x > 0 and y < 0:
-            return 'Cuadrante 4'
-    
+            print('Cuadrante 4')
+    print()
     def vector(self, punto):
         x = punto.x - self.x
         y = punto.y - self.y
         print(f'vector de ({str(self.x)},{str(self.y)}) y ({str(punto.x)},{str(punto.y)})')
         print(f'es ({str(x)},{str(y)})')
-
+    print()
     def distancia(self, punto):
         d = math.sqrt(((punto.x - self.x)**2) + ((punto.y - self.y)**2))
         print(f'La distancia entre ({str(self.x)},{str(self.y)}) y ({str(punto.x)},{str(punto.y)}')
         print(f'es de {d}')
-    
+    print()
 
 class Rectangulo:
     def __init__(self, inicial = Punto(), final = Punto()):
@@ -49,17 +49,18 @@ class Rectangulo:
         final, inicial = self.final.x, self.inicial.x
         base = abs(final - inicial)
         print(base)
-    
+        return base
+    print()
     def altura_rectangulo(self):
         final, inicial = self.final.y, self.inicial.y
         altura = abs(final - inicial)
         print(altura)
-    
+        return altura
+    print()
     def area_rectangulo(self):
-        base = self.base()
-        altura = self.altura()
-        area = base * altura
-        return area
+        base = self.base_rectangulo()
+        altura = self.altura_rectangulo()
+        print(f'el area del rectangulo es de {base * altura}')
 
 A = Punto(2,3)
 B = Punto(5,5)
